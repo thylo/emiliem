a7304da07695872f4e5851cedc24a8e69df635f5
Julien Moreau <moreaujulien86@gmail.com>
2018-02-28 20:27:06 +0100

    Move files around

--- static.config.js
+++ static.config.js
@@ -1,62 +1,100 @@
-import axios from 'axios'
-import React, { Component } from 'react'
-import { ServerStyleSheet } from 'styled-components'
+import axios from "axios";
+import React, { Component } from "react";
+import { ServerStyleSheet } from "styled-components";
+
+import fs from "fs";
+import klaw from "klaw";
+import path from "path";
+import matter from "gray-matter";
+
+function getPosts() {
+  const items = [];
+  // Walk ("klaw") through posts directory and push file paths into items array //
+  const getFiles = () =>
+    new Promise(resolve => {
+      // Check if posts directory exists //
+      if (fs.existsSync("./src/pages")) {
+        klaw("./src/pages")
+          .on("data", item => {
+            // Filter function to retrieve .md files //
+            if (path.extname(item.path) === ".md") {
+              // If markdown file, read contents //
+              const data = fs.readFileSync(item.path, "utf8");
+              // Convert to frontmatter object and markdown content //
+              const dataObj = matter(data);
+              // Create slug for URL //
+              dataObj.data.slug = dataObj.data.title
+                .toLowerCase()
+                .replace(/ /g, "-")
+                .replace(/[^\w-]+/g, "");
+              // Remove unused key //
+              delete dataObj.orig;
+              // Push object into items array //
+              items.push(dataObj);
+            }
+          })
+          .on("error", e => {
+            console.log(e);
+          })
+          .on("end", () => {
+            // Resolve promise for async getRoutes request //
+            // posts = items for below routes //
+            resolve(items);
+          });
+      } else {
+        // If src/posts directory doesn't exist, return items as empty array //
+        resolve(items);
+      }
+    });
+  return getFiles();
+}
 
 export default {
   getSiteData: () => ({
-    title: 'React Static',
+    title: "React Static"
   }),
   getRoutes: async () => {
-    const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    const pages = await getPosts();
+    console.log(pages);
     return [
       {
-        path: '/',
-        component: 'src/containers/Home',
+        path: "/",
+        component: "src/containers/Home"
       },
       {
-        path: '/about',
-        component: 'src/containers/About',
+        path: "/about",
+        component: "src/containers/About"
       },
-      {
-        path: '/blog',
-        component: 'src/containers/Blog',
-        getData: () => ({
-          posts,
-        }),
-        children: posts.map(post => ({
-          path: `/post/${post.id}`,
-          component: 'src/containers/Post',
-          getData: () => ({
-            post,
-          }),
-        })),
-      },
+
       {
         is404: true,
-        component: 'src/containers/404',
-      },
-    ]
+        component: "src/containers/404"
+      }
+    ];
   },
   renderToHtml: (render, Comp, meta) => {
-    const sheet = new ServerStyleSheet()
-    const html = render(sheet.collectStyles(<Comp />))
-    meta.styleTags = sheet.getStyleElement()
-    return html
+    const sheet = new ServerStyleSheet();
+    const html = render(sheet.collectStyles(<Comp />));
+    meta.styleTags = sheet.getStyleElement();
+    return html;
   },
   Document: class CustomHtml extends Component {
-    render () {
-      const { Html, Head, Body, children, renderMeta } = this.props
+    render() {
+      const { Html, Head, Body, children, renderMeta } = this.props;
 
       return (
         <Html>
           <Head>
             <meta charSet="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1"
+            />
             {renderMeta.styleTags}
           </Head>
           <Body>{children}</Body>
         </Html>
-      )
+      );
     }
-  },
-}
+  }
+};
