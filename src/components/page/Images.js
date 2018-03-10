6a24abe7b0f7c9abe5e3186295b26fed7aad213b
Julien Moreau <moreaujulien86@gmail.com>
2018-03-10 21:20:42 +0100

    fix

--- src/components/page/Images.js
+++ src/components/page/Images.js
@@ -9,6 +9,12 @@ const Image = styled(DuotoneImage)`
   clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
 `;
 
+const BaseImage = styled.img`
+  margin: 0;
+  -webkit-clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
+  clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
+`;
+
 const ImageWrapper = styled.div`
   position: relative;
 `;
@@ -17,14 +23,19 @@ export const DiamondImage = ({ image, size }) => {
   if (!image) return <div />;
   return (
     <ImageWrapper>
-      {image && (
-        <Image
-          src={`/images/${size}-${image.substr(1)}`}
-          alt=""
-          primaryColor={beige}
-          secondaryColor={darkGreen}
-        />
-      )}
+      {image &&
+        window && (
+          <Image
+            src={`/images/${size}-${image.substr(1)}`}
+            alt=""
+            primaryColor={beige}
+            secondaryColor={darkGreen}
+          />
+        )}
+      {image &&
+        !window && (
+          <BaseImage src={`/images/${size}-${image.substr(1)}`} alt="" />
+        )}
     </ImageWrapper>
   );
 };
